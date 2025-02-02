7eb98329067182bd8a043194bd6ca96f7d9453be
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-02-01 19:01:10 -0500

    feat: Improve content in src/reporepo/main.py to support new arguments to cli and functions.

--- src/reporepo/main.py
+++ src/reporepo/main.py
@@ -1,5 +1,8 @@
 """Main module for the reporepo command-line interface."""
 
+import json
+from enum import Enum
+
 import requests
 import typer
 from rich.console import Console
@@ -13,42 +16,95 @@ app = Typer()
 console = Console()
 
 
-def change_user_access(repo_url: str, username: str, token: str):
+class StatusCode(Enum):
+    """Define the status codes for the GitHub API."""
+
+    SUCCESS = 204
+    BAD_REQUEST = 400
+    UNAUTHORIZED = 401
+    FORBIDDEN = 403
+    NOT_FOUND = 404
+    UNPROCESSABLE_ENTITY = 422
+    INTERNAL_SERVER_ERROR = 500
+
+
+class GitHubAccessLevel(Enum):
+    """Define the access levels for GitHub repositories."""
+
+    READ = "read"
+    TRIAGE = "triage"
+    WRITE = "write"
+    MAINTAIN = "maintain"
+    ADMIN = "admin"
+
+
+def print_json_string(json_string: str):
+    """Convert JSON string to dictionary and print each key-value pair."""
+    dictionary = json.loads(json_string)
+    for key, value in dictionary.items():
+        console.print(f"\t{key}: {value}")
+
+
+def modify_user_access(
+    github_organization_url: str,
+    repo_prefix: str,
+    username: str,
+    access_level: GitHubAccessLevel,
+    token: str,
+) -> None:
     """Change user access to read."""
     # extract the repository name from the URL
-    repo_name = repo_url.split("github.com/")[1]
-    # GitHub API URL to add a collaborator
-    api_url = (
-        f"https://api.github.com/repos/{repo_name}/collaborators/{username}"
-    )
+    organization_name = github_organization_url.split("github.com/")[1].split(
+        "/"
+    )[0]
+    console.print(organization_name)
+    full_repository_name = repo_prefix + "-" + username
+    full_name_for_api = organization_name + "/" + full_repository_name
+    # define the API URL for the request
+    api_url = f"https://api.github.com/repos/{full_name_for_api}/collaborators/{username}"
     # headers for the request
     headers = {
         "Authorization": f"token {token}",
         "Accept": "application/vnd.github.v3+json",
     }
     # data for the request
-    data = {"permission": "read"}
+    data = {"permission": access_level.value}
     # make the PUT request to change the user's permission
     response = requests.put(api_url, headers=headers, json=data)
     # check if the request was successful
-    if response.status_code == 204:
-        console.print(f"successfully changed {username}'s access to 'read'.")
+    # display positive configuration since change of the access level worked
+    if response.status_code == StatusCode.SUCCESS.value:
+        console.print(f"󰄬 Changed {username}'s access to 'read'")
+    # display error message since the change of the access level did not work
     else:
         console.print(
-            f"failed to change access: {response.status_code} - {response.text}"
+            f" Failed to change access for {username}: {response.status_code}"
         )
+        print_json_string(response.text)
 
 
 @app.command()
 def cli(
-    repo_url: str = typer.Argument(..., help="The URL of the repository"),
+    github_org_url: str = typer.Argument(
+        ..., help="The URL of GitHub organization"
+    ),
+    repo_prefix: str = typer.Argument(
+        ..., help="The prefix for GitHub repository"
+    ),
     username: str = typer.Argument(
-        ..., help="The username to change access for"
+        ..., help="The username subject to access change"
     ),
     token: str = typer.Argument(
         ..., help="The GitHub token for authentication"
     ),
+    access_level: GitHubAccessLevel = typer.Option(
+        GitHubAccessLevel.READ.value,
+        help="The access level for user",
+    ),
 ):
-    """Define command-line interface for the apparent program."""
-    console.print("Hello, world!")
-    change_user_access(repo_url, username, token)
+    """Define command-line interface for the reporepo program."""
+    console.print(":sparkles: RepoRepo helps you `repo` a GitHub repository!")
+    console.print()
+    modify_user_access(
+        github_org_url, repo_prefix, username, access_level, token
+    )
