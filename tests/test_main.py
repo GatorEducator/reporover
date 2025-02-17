a9ba641451cdbc6cc119790445e4106876b45c0b
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-02-17 15:43:06 -0500

    test: Use a better name for the test case in the test_main.py file.

--- tests/test_main.py
+++ tests/test_main.py
@@ -7,7 +7,7 @@ from reporepo.main import app
 runner = CliRunner()
 
 
-def test_cli():
+def test_cli_provides_help_no_error():
     """Ensure that the CLI interface is working as expected when run with --help."""
     result = runner.invoke(app, ["--help"])
     assert result.exit_code == 0
