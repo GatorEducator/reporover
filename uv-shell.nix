defd7ffeb46209117d0c20e801ee99767a1354b1
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-03-31 09:11:20 -0400

    chore: Add more libraries to the uv-shell.nix to handle installation and running problems.

--- uv-shell.nix
+++ uv-shell.nix
@@ -6,9 +6,23 @@ let
   unstable = import <nixos-unstable> { config =  baseconfig; };
 in
 pkgs.mkShell {
-  buildInputs = with pkgs; [ unstable.uv unstable.python312 stdenv.cc.cc.lib ];
+  buildInputs = with pkgs; [
+    unstable.uv
+    unstable.python312
+    stdenv.cc.cc.lib
+    zlib
+    # Additional libraries for building C extensions
+    openssl
+    glib
+    libffi
+    ncurses
+    # Development tools
+    gcc
+    gnumake
+    pkg-config
+  ];
   shellHook = ''
-    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib:$LD_LIBRARY_PATH
+    export LD_LIBRARY_PATH=${stdenv.cc.cc.lib}/lib:${zlib}/lib:${openssl.out}/lib:${pkgs.libffi}/lib:${ncurses}/lib:$LD_LIBRARY_PATH
     export UV_PYTHON=${pkgs.python312}
     uv venv
   '';
