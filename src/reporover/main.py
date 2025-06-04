7bc9724f0ed660d4282996a871d2023f4127efe5
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-06-04 13:56:13 -0400

    refactor: Move the user access function out of src/reporover/main.py to src/reporover/user.py.

--- src/reporover/main.py
+++ src/reporover/main.py
@@ -5,7 +5,7 @@ import csv
 import json
 from datetime import datetime
 from pathlib import Path
-from typing import Callable, List, Optional, Union
+from typing import List, Optional, Union
 
 import requests
 import typer
@@ -21,6 +21,7 @@ from reporover.constants import (
     PullRequestMessages,
     StatusCode,
 )
+from reporover.user import modify_user_access
 from reporover.util import print_json_string, read_usernames_from_json
 
 # define the Typer app that will be used
@@ -37,61 +38,6 @@ def display_welcome_message() -> None:
     console.print(
         ":sparkles: RepoRover manages and analyzes remote GitHub repositories! Arf!"
     )
-
-
-def modify_user_access(  # noqa: PLR0913
-    github_organization_url: str,
-    repo_prefix: str,
-    username: str,
-    access_level: GitHubAccessLevel,
-    token: str,
-    progress: Progress,
-    put_request_function: Callable = requests.put,
-) -> Union[StatusCode, None]:
-    """Change user access to read."""
-    # define the status codes for the request
-    request_status_code = None
-    # extract the repository name from the URL
-    organization_name = github_organization_url.split("github.com/")[1].split(
-        "/"
-    )[0]
-    # define the full name of the repository that involves
-    # the prefix of the repository a separating dash and
-    # then the name of the user; note that this is the standard
-    # adopted by GitHub repositories created by GitHub Classroom
-    full_repository_name = repo_prefix + "-" + username
-    full_name_for_api = organization_name + "/" + full_repository_name
-    # define the API URL for the request
-    api_url = f"https://api.github.com/repos/{full_name_for_api}/collaborators/{username}"
-    # headers for the request
-    headers = {
-        "Authorization": f"token {token}",
-        "Accept": "application/vnd.github.v3+json",
-    }
-    # data for the request
-    data = {"permission": access_level.value}
-    # make the PUT request to change the user's permission
-    response = put_request_function(api_url, headers=headers, json=data)
-    # check if the request was successful
-    # display positive configuration since change of the access level worked
-    if response.status_code == StatusCode.SUCCESS.value:
-        progress.console.print(
-            f"󰄬 Changed {username}'s access to '{access_level.value}' in"
-            + f" {full_repository_name}"
-        )
-        request_status_code = StatusCode.SUCCESS
-    # display error message since the change of the access level did not work
-    else:
-        # display the basic error message
-        progress.console.print(
-            f" Failed to change {username}'s access to '{access_level.value}' in"
-            + f" {full_repository_name}\n"
-            + f"  Diagnostic: {response.status_code}"
-        )
-        # display all of the rest of the details in the string
-        # that encodes the JSON response from the GitHub API
-        print_json_string(response.text, progress)
-    return request_status_code
 
 
 def leave_pr_comment(  # noqa: PLR0913
