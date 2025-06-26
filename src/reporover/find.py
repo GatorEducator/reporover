607b2ded30e17b5963dde639a7e10a09450f1a3e
Gregory M. Kapfhammer <gkapfham@allegheny.edu>
2025-06-26 17:53:51 -0400

    style: Improve the source code formatting in the src/reporover/find.py file.

--- src/reporover/find.py
+++ src/reporover/find.py
@@ -60,13 +60,9 @@ def find_repositories(  # noqa: PLR0913
         if forks:
             console.print(f":mag: Repository minimum forks: {forks}")
         if created_after:
-            console.print(
-                f":mag: Created after: {created_after} (YYYY-MM-DD)"
-            )
+            console.print(f":mag: Created after: {created_after} (YYYY-MM-DD)")
         if updated_after:
-            console.print(
-                f":mag: Updated after: {updated_after} (YYYY-MM-DD)"
-            )
+            console.print(f":mag: Updated after: {updated_after} (YYYY-MM-DD)")
         # filter repositories based on the provided criteria
         filtered_repositories = _filter_repositories_by_criteria(
             repositories,
